c94e2ccfdb9cb62a4003f6dbfceec7f29d053f5e
xiaojun <xiaojun@xszq.net>
2019-10-17 09:53:25 +0800

    Create ApolloConfig.go

---
+++ config/ApolloConfig.go
@@ -0,0 +1,55 @@
+package config
+
+import (
+	"fmt"
+	"github.com/shima-park/agollo"
+	"log"
+)
+
+type Conf interface  {
+	agollo.Agollo
+}
+
+var LogLevel = "debug"
+
+func InitConfig(configServerURL, appID string) agollo.Agollo {
+
+	log.Println("appID:", appID)
+	res, err := agollo.New(configServerURL, appID, agollo.AutoFetchOnCacheMiss())
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
+
+func LoadApolloWatch(Config agollo.Agollo, loadConfig func(Config Conf)) {
+	loadConfig(Config)
+	errorCh := Config.Start()
+
+	// 监听apollo配置更改事件
+	// 返回namespace和其变化前后的配置,以及可能出现的error
+	watchCh := Config.Watch()
+
+	stop := make(chan bool)
+	watchNamespace := "application"
+	watchNSCh := Config.WatchNamespace(watchNamespace, stop)
+
+	go func() {
+		for {
+			select {
+			case err := <-errorCh:
+				fmt.Println("Error:", err)
+			case <-watchCh:
+				loadConfig(Config)
+			case <-watchNSCh:
+				//fmt.Println("Watch Namespace", watchNamespace, resp)
+			}
+		}
+	}()
+
+	select {}
+}
+
+func GetConf(Config Conf, key, defaultValue string) string {
+	return Config.Get(key, agollo.WithDefault(defaultValue))
+}
